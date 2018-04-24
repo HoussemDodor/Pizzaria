e8f66467834aa3ee66f35b59989c633320011b28
Houssem <houssem-98@hotmail.com>
2018-04-24 15:16:35 +0200

    DAL klasses erin gezet nog niet geimplementeerd.

--- PizzariaWebApp/DataAccesLayer/SQLContext/DoughTypeSQLContext.cs
+++ PizzariaWebApp/DataAccesLayer/SQLContext/DoughTypeSQLContext.cs
@@ -10,14 +10,60 @@ namespace DataAccesLayer
 {
     internal class DoughTypeSQLContext : IDoughType
     {
+        private DatabaseConnection dbconn = new DatabaseConnection();
+        DoughType doughType;
+
         public List<DoughType> GetAllDoughTypes()
         {
-            throw new NotImplementedException();
+            List<DoughType> DoughTypeList = new List<DoughType>();
+            string query = "SELECT * FROM DoughType";
+            using (SqlConnection conn = dbconn.Connect)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            DoughTypeList.Add(CreateDoughTypeFromReader(reader));
+                        }
+                    }
+                }
+                conn.Close();
+                return DoughTypeList;
+            }
         }
 
         public DoughType GetDoughTypeByID(int ID)
         {
-            throw new NotImplementedException();
+            string query = "SELECT * FROM DoughType WHERE ID = @id";
+            using (SqlConnection conn = dbconn.Connect)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", ID);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            doughType = CreateDoughTypeFromReader(reader);
+                        }
+                    }
+                }
+                conn.Close();
+                return doughType;
+            }
+        }
+
+        private DoughType CreateDoughTypeFromReader(SqlDataReader reader)
+        {
+            return new DoughType
+                (
+                    Convert.ToInt32(reader["ID"]),
+                    Convert.ToString(reader["Name"]),
+                    Convert.ToDecimal(reader["Price"]),
+                    Convert.ToDecimal(reader["BuyInPrice"])
+                );
         }
     }
 }
