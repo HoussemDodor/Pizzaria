297e2d49c43b7101d526d044b41e991db2ddc864
Houssem <houssem-98@hotmail.com>
2018-05-29 18:11:35 +0200

    Webdeploy met css toegevoegd. Category sql context geimplementeerd

--- PizzariaWebApp/DataAccesLayer/SQLContext/CategorySQLContext.cs
+++ PizzariaWebApp/DataAccesLayer/SQLContext/CategorySQLContext.cs
@@ -4,19 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models;
+using System.Data.SqlClient;
 
 namespace DataAccesLayer
 {
     internal class CategorySQLContext : ICategory
     {
+        DatabaseConnection dbconn = new DatabaseConnection();
+        Category category;
+
         public List<Category> GetAllCategory()
         {
-            throw new NotImplementedException();
+            List<Category> categorys = new List<Category>();
+            string query = "SELECT * FROM Category";
+            using (SqlConnection conn = dbconn.Connect)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            categorys.Add(CreateCategoryFromReader(reader));
+                        }
+                    }
+                }
+                conn.Close();
+                return categorys;
+            }
         }
 
         public Category GetCategoryByID(int categoryID)
         {
-            throw new NotImplementedException();
+            string query = "SELECT * FROM Category WHERE ID = @id";
+            using (SqlConnection conn = dbconn.Connect)
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", categoryID);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            category = CreateCategoryFromReader(reader);
+                        }
+                    }
+                }
+                conn.Close();
+                return category;
+            }
+        }
+
+        public Category CreateCategoryFromReader(SqlDataReader reader)
+        {
+            return new Category()
+            {
+                ID = Convert.ToInt32(reader["ID"]),
+                name = Convert.ToString(reader["Name"])
+            };
         }
     }
 }
